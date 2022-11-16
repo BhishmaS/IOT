3a8c322223774673cb944a74069bddd5a6ac46fd
Bhishma Sukhavasi <bhishma.sukhavasi@gmail.com>
2022-11-15 20:42:48 -0800

    included data UI representation using python flask

---
+++ IoT_Flask/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pymysql
+
+app = Flask(__name__)
+
+# @app.route('/')
+@app.route('/', methods=['GET'])
+def hello_world():
+
+    db = pymysql.connect(host="localhost", user="root", password="root", db="iot")
+    cursor = db.cursor()
+    query = "SELECT TIMESTAMPDIFF(SECOND, '1970-01-01 00:00:00.00', dateTime) * 1000 as utcSeconds," + \
+            "CAST(value AS FLOAT) AS value FROM iot.devicelogs ORDER BY dateTime"
+
+    cursor.execute(query)
+    data = cursor.fetchall()
+
+    print(data)
+    sensor_data = data
+
+    # This is where we pass the sensor data to index.html
+    return render_template('index.html', trendData=sensor_data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
